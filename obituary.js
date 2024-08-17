30b4bb8d344fcba0c30f1626a4ccb3585b559da7
Shiangan <avarubby85110@gmail.com>
2024-08-17 23:18:36 +0800

    Update obituary.js

--- obituary.js
+++ obituary.js
@@ -20,6 +20,25 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+function plusSlides(n) {
+    slideIndex += n;
+    showSlides();
+}
+
     // 留言板提交功能
     const commentForm = document.getElementById('comment-form');
     const commentsContainer = document.getElementById('comments-container');
